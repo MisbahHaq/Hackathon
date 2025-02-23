0623efc17d8c462fe641c3fe77ac3f1895bad8ef
Misbah <misbahmuneer2003@gmail.com>
2025-02-23 09:18:24 +0500

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hack/Pages/home_Page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false);
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
